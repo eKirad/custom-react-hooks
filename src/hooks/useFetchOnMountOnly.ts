99c3107ef2e0d2edddce75e0de61559394cbc2c3
eKirad <evkirad@gmail.com>
2024-10-24 12:20:46 +0300

    :art: Adds useEffect only on update hook

---
+++ src/hooks/useFetchOnMountOnly.ts
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from 'react'
+
+type Args<T> = {
+    dependencies: Array<T>
+    callback: () => void
+}
+
+export const useEffectOnUpdateOnly = <T>({ dependencies, callback }: Args<T>) => {
+    const isInitialRender = useRef(true)
+
+    useEffect(() => {
+        if (isInitialRender.current) {
+            isInitialRender.current = false
+            return
+        }
+
+        callback()
+    }, [...dependencies])
+}
