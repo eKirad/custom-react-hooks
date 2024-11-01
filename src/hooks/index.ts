60925eafcfaf9333622d00f51f03b7b1edb89531
eKirad <evkirad@gmail.com>
2024-11-01 15:22:50 +0200

    :truck: Adds custom hooks to export

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,6 @@
+import { useEffectOnUpdateOnly } from './custom-use-effects/useEffectOnUpdateOnly'
+import { useQuery } from './data-fetching/useQuery'
 import { useFetchAll } from './data-fetching/useFetchAll'
 import { useFetchOne } from './data-fetching/useFetchOne'
 
-export { useFetchAll, useFetchOne }
+export { useFetchAll, useFetchOne, useQuery, useEffectOnUpdateOnly }
