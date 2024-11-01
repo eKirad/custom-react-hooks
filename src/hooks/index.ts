911d140546712b9bbe385b66da28f3b4bc93658d
eKirad <evkirad@gmail.com>
2024-11-01 15:18:09 +0200

    :truck: Updates imports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,4 @@
-import { useFetchAll } from './useFetchAll'
-import { useFetchOne } from './useFetchOne'
+import { useFetchAll } from './data-fetching/useFetchAll'
+import { useFetchOne } from './data-fetching/useFetchOne'
 
 export { useFetchAll, useFetchOne }
