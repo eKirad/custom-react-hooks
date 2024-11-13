9e6d3bcaecb92df7097747dfbb8ecde9584c73d8
eKirad <evkirad@gmail.com>
2024-11-13 16:07:51 +0200

    :truck: Renames use effect on update only hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,4 @@
-import { useEffectOnUpdateOnly } from './custom-use-effects/useEffectOnUpdateOnly'
+import { useEffectOnUpdateOnly } from './custom-use-effects/use-effect-on-update-only'
 import { useQuery } from './data-fetching/useQuery'
 import { useFetchAll } from './data-fetching/useFetchAll'
 import { useFetchOne } from './data-fetching/useFetchOne'
