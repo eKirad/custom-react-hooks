9303d3c4f69bff951d3217d266dc98488ea10aeb
eKirad <evkirad@gmail.com>
2024-11-13 16:10:53 +0200

    :truck: Updates hooks

---
+++ src/hooks/data-fetching/use-query.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import { UseQueryArgs } from '../../types/useQueryArgs'
+
+export const useQuery = <T>({ initialData, queryKey, queryFn }: UseQueryArgs<T>) => {
+    const [data, setData] = useState<T>(initialData)
+    const [isLoading, setIsLoading] = useState(false)
+    const [isError, setIsError] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true)
+            try {
+                const responseData = await queryFn()
+                setData(responseData)
+                setIsError(false)
+            } catch (error) {
+                console.error(error)
+                setIsError(true)
+            }
+
+            setIsLoading(false)
+        }
+
+        fetchData()
+    }, [...queryKey])
+
+    return [{ data, isLoading, isError }]
+}
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,6 @@
 import { useEffectOnUpdateOnly } from './custom-use-effects/use-effect-on-update-only'
-import { useQuery } from './data-fetching/useQuery'
-import { useFetchAll } from './data-fetching/useFetchAll'
-import { useFetchOne } from './data-fetching/useFetchOne'
+import { useQuery } from './data-fetching/use-query'
+import { useFetchAll } from './data-fetching/use-fetch-all'
+import { useFetchOne } from './data-fetching/use-fetch-one'
 
 export { useFetchAll, useFetchOne, useQuery, useEffectOnUpdateOnly }
