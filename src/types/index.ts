d437e74f756935ad76db6d0d1a354b786df516c5
eKirad <evkirad@gmail.com>
2024-11-13 16:54:52 +0200

    :bug: Adds proper type to index

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
-import { UseQueryArgs } from './../../dist/esm/types/useQueryArgs.d'
 import { QueryParams } from './queryParams.types.'
 import { FetchDataServiceInterface } from './service.types.'
+import { UseQueryArgs } from './useQueryArgs.types'
 
 export { QueryParams, FetchDataServiceInterface, UseQueryArgs }
