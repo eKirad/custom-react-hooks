7a203d3ea999cf2949ce9e721ad90d71af526d63
eKirad <evkirad@gmail.com>
2024-11-13 23:40:38 +0200

    :art: Updates hook export

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,6 @@
-import { useEffectOnUpdateOnly } from './custom-use-effects/use-effect-on-update-only'
 import { useQuery } from './data-fetching/use-query'
 import { useFetchAll } from './data-fetching/use-fetch-all'
 import { useFetchOne } from './data-fetching/use-fetch-one'
+import { useEffectOnUpdateOnly } from './custom-use-effects/use-effect-on-update-only'
 
 export { useFetchAll, useFetchOne, useQuery, useEffectOnUpdateOnly }
