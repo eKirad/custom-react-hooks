dfe69bf74699f413eb203b2df3e3ef0e1fb2227d
eKirad <evkirad@gmail.com>
2024-11-13 16:37:10 +0200

    :recycle: Removes horizontal space

--- src/types/service.types..ts
+++ src/types/service.types..ts
@@ -1,5 +1,4 @@
 import { QueryParams } from '.'
-
 export interface FetchDataServiceInterface {
     fetchAll<T>(uri: string, queryParams: QueryParams): Promise<Array<T>>
     fetchOne<T>(uri: string, id: string): Promise<T>
