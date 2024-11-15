5f3ce3fdeeadcabb8f8c5e9948c8a42bc6113741
eKirad <evkirad@gmail.com>
2024-11-15 14:00:53 +0200

    :bug: Adds storage fallback

--- src/hooks/misc/use-local-storage.ts
+++ src/hooks/misc/use-local-storage.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 
 export const useLocalStorage = (key: string) => {
-    const [value, setValue] = useState(localStorage.getItem(JSON.parse(key)))
+    const [value, setValue] = useState(localStorage.getItem(JSON.parse(key)) ?? '')
 
     useEffect(() => localStorage.setItem(key, JSON.stringify(value)), [key, value])
 
