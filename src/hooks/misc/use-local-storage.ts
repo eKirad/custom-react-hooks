ec6c5590fe7e41215693fa489e526a26ed9537ad
eKirad <evkirad@gmail.com>
2024-11-15 14:19:15 +0200

    :label: Extracts local storage fallback

--- src/hooks/misc/use-local-storage.ts
+++ src/hooks/misc/use-local-storage.ts
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
+import { LOCAL_STORAGE_FALLBACK_VALUE } from '../../consts'
 
 export const useLocalStorage = (key: string) => {
-    const [value, setValue] = useState(JSON.parse(localStorage.getItem(key) ?? '"{}"'))
+    const [value, setValue] = useState(JSON.parse(localStorage.getItem(key) ?? LOCAL_STORAGE_FALLBACK_VALUE))
 
     useEffect(() => localStorage.setItem(key, JSON.stringify(value)), [key, value])
 
