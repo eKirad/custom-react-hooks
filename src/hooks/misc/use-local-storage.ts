f56f5597314323d5d90a3d60fa3087713c11bfd4
eKirad <evkirad@gmail.com>
2024-11-15 14:09:44 +0200

    :bug: Fixes local storage access

--- src/hooks/misc/use-local-storage.ts
+++ src/hooks/misc/use-local-storage.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 
 export const useLocalStorage = (key: string) => {
-    const [value, setValue] = useState(JSON.parse(localStorage.getItem(key) ?? ''))
+    const [value, setValue] = useState(JSON.parse(localStorage.getItem(key)) ?? '')
 
     useEffect(() => localStorage.setItem(key, JSON.stringify(value)), [key, value])
 
