8d4ac7859709536ac2776920468e5d4edacaa4d0
eKirad <evkirad@gmail.com>
2024-11-17 22:14:35 +0200

    :tada: Adds use-click-outside hook

---
+++ src/hooks/dom/use-outside-scroll.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react'
+
+type Args = {
+    callback: () => void
+}
+
+export const useClickOutside = ({ callback }: Args) => {
+    const ref = useRef<HTMLElement | null>(null)
+
+    useEffect(() => {
+        const handleClick = (event: Event) => {
+            const isNotWithinRef = ref.current && !ref.current.contains(event.target as Node)
+            if (isNotWithinRef) callback()
+        }
+
+        window.addEventListener('click', handleClick)
+
+        return () => window.removeEventListener('click', handleClick)
+    }, [callback])
+
+    return ref
+}
