ce6337c611c0a2000e604b3866cb5de031567822
eKirad <evkirad@gmail.com>
2024-11-19 18:20:38 +0200

    :sparkles: Adds use debounce hook

---
+++ src/hooks/misc/use-debounce.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react'
+
+export const useDebounce = (value: string, delay: number) => {
+    const [debounceValue, setDebounceValue] = useState(value)
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setDebounceValue(value), delay)
+
+        return () => clearTimeout(timeout)
+    }, [value])
+
+    return debounceValue
+}
