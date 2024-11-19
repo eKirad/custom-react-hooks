439393d8ae710ebd364213260327227b67acaa45
eKirad <evkirad@gmail.com>
2024-11-19 22:24:56 +0200

    :label: Adds JS value types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
+import { JSValueType } from './jsValueTypes.type'
 import { HTMLRefType } from './htmlRef.type'
 import { QueryParams } from './queryParams.types.'
 import { FetchDataServiceInterface } from './service.types.'
 
-export { QueryParams, FetchDataServiceInterface, HTMLRefType }
+export { QueryParams, FetchDataServiceInterface, HTMLRefType, JSValueType }
