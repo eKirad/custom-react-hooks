09afb076831695bd4b9f3342aa0ae02abebdb77c
eKirad <evkirad@gmail.com>
2024-12-14 14:55:40 +0200

    :art: Adds initial value to use form input hook

--- src/hooks/dom/use-form-input.ts
+++ src/hooks/dom/use-form-input.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-export const useFormInput = (initialFormFieldValue: string) => {
+export const useFormInput = (initialFormFieldValue: string = '') => {
     const [formField, setFormField] = useState(initialFormFieldValue)
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => setFormField(event.target.value)
